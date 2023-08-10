31462183b50ba41b31baa5fbbaaecb20cb5adff3
Hrithik <slumdog4321@gmail.com>
2023-08-10 16:29:39 +0530

    Project Setup and Initializaton

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-semibold text-white">Hrihtik's Portfolio</h1>
+      <h2 className="text-2xl font-bold text-white mt-10">
+        Just the First Step to Creating The Masterpiece
+      </h2>
+    </>
+  );
+}
+
+export default App;
